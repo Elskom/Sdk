e051bd7e957b1ab68c6a31da506cd4f8ce775247
AraHaan <seandhunt_7@yahoo.com>
2021-07-28 21:54:21 -0400

    Use xplat minidump apis. (#243)
    Signed-off-by: AraHaan <seandhunt_7@yahoo.com>

--- SettingsFile/SettingsFile/SettingsFile.cs
+++ SettingsFile/SettingsFile/SettingsFile.cs
@@ -8,13 +8,39 @@ namespace Elskom.Generic.Libs
     using System;
     using System.Diagnostics;
     using System.IO;
-    using System.Text;
+    using System.Runtime.InteropServices;
 
     /// <summary>
     /// A class that handles the settings for any application.
     /// </summary>
     public static class SettingsFile
     {
+        static SettingsFile()
+        {
+            using var thisProcess = Process.GetCurrentProcess();
+            ThisProcessName = thisProcess.ProcessName;
+            ThisProcessId = thisProcess.Id;
+            SettingsJson = JsonSettings.OpenFile();
+            ApplicationName = "Els_kom";
+
+            // We cannot use System.Windows.Forms.Application.LocalUserAppDataPath as it would
+            // Create annoying folders, and throw annoying Exceptions making it harder to
+            // debug as it spams the debugger. Also then we would not need to Replace
+            // everything added to the path obtained from System.Environment.GetFolderPath.
+            // Also trap devenv if it is detected as well and use the provided ApplicationName instead.
+            var localApplicationDataFolder = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.DoNotVerify)}{Path.DirectorySeparatorChar}{(ThisProcessName == "devenv" ? ApplicationName : ThisProcessName)}{Path.DirectorySeparatorChar}";
+            if (!Directory.Exists(localApplicationDataFolder))
+            {
+                _ = Directory.CreateDirectory(localApplicationDataFolder);
+            }
+
+            SettingsPath = $"{localApplicationDataFolder}Settings.json";
+            ErrorLogPath = $"{localApplicationDataFolder}{ThisProcessName}-{ThisProcessId}.log";
+
+            // On Non-Windows OS's all crash dumps must be named "core.{PID}"!!!
+            MiniDumpPath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? $"{localApplicationDataFolder}{ThisProcessName}-{ThisProcessId}.mdmp" : $"{localApplicationDataFolder}core.{ThisProcessId}";
+        }
+
         /// <summary>
         /// Gets or sets the Json settings file instance.
         ///
@@ -27,13 +53,13 @@ namespace Elskom.Generic.Libs
         /// This is designed so there is globally only
         /// a single instance to save time, and memory.
         /// </value>
-        public static JsonSettings SettingsJson { get; set; } = JsonSettings.OpenFile();
+        public static JsonSettings SettingsJson { get; set; }
 
         /// <summary>
         /// Gets or sets the Application's name to use for Resolving the paths for the
         /// Settings file, error logs, and minidumps.
         /// </summary>
-        public static string ApplicationName { get; set; } = "Els_kom";
+        public static string ApplicationName { get; set; }
 
         /// <summary>
         /// Gets the path to the Application Settings file.
@@ -45,59 +71,22 @@ namespace Elskom.Generic.Libs
         ///
         /// Creates the folder if needed.
         /// </value>
-        public static string SettingsPath
-            => PrivatePathResolver(".json");
+        public static string SettingsPath { get; }
 
         /// <summary>
         /// Gets the path to the Application Error Log file.
         ///
         /// Creates the Error Log file if needed.
         /// </summary>
-        public static string ErrorLogPath
-            => PrivatePathResolver(".log");
+        public static string ErrorLogPath { get; }
 
         /// <summary>
         /// Gets the path to the Application Mini-Dump file.
         /// </summary>
-        public static string MiniDumpPath
-            => PrivatePathResolver(".mdmp");
+        public static string MiniDumpPath { get; }
 
-        private static string PrivatePathResolver(string fileExtension)
-        {
-            // We cannot use System.Windows.Forms.Application.LocalUserAppDataPath as it would
-            // Create annoying folders, and throw annoying Exceptions making it harder to
-            // debug as it spams the debugger. Also then we would not need to Replace
-            // everything added to the path obtained from System.Environment.GetFolderPath.
-            StringBuilder localPath = new();
-            _ = localPath.Append(
-                Environment.GetFolderPath(
-                    Environment.SpecialFolder.LocalApplicationData));
-            using var thisProcess = Process.GetCurrentProcess();
-            _ = localPath.Append($"{Path.DirectorySeparatorChar}{thisProcess.ProcessName}");
-            if (!Directory.Exists(localPath.ToString()))
-            {
-                _ = Directory.CreateDirectory(localPath.ToString());
-            }
+        internal static string ThisProcessName { get; }
 
-            if (fileExtension.Equals(".json", StringComparison.Ordinal))
-            {
-                // do not create the settings file, just pass this path to the json
-                // interface for settings. if we create it ourselves the optimized
-                // class will fail to work right if it is empty.
-                _ = localPath.Append($"{Path.DirectorySeparatorChar}Settings.json");
-            }
-            else if (fileExtension.Equals(".log", StringComparison.Ordinal))
-            {
-                _ = localPath.Append($"{Path.DirectorySeparatorChar}{thisProcess.ProcessName}-{thisProcess.Id}.log");
-            }
-            else if (fileExtension.Equals(".mdmp", StringComparison.Ordinal))
-            {
-                _ = localPath.Append($"{Path.DirectorySeparatorChar}{thisProcess.ProcessName}-{thisProcess.Id}.mdmp");
-            }
-
-            // trap devenv if it is detected.
-            localPath = localPath.Replace("devenv", ApplicationName);
-            return localPath.ToString();
-        }
+        internal static int ThisProcessId { get; }
     }
 }
--- newsmake/newsmake/newsmake/Program.cs
+++ newsmake/newsmake/newsmake/Program.cs
@@ -18,11 +18,12 @@ namespace Newsmake
     using System.Text;
     using System.Threading.Tasks;
     using Elskom.Generic.Libs;
+    using Microsoft.Diagnostics.NETCore.Client;
     using newsmakeResources = Elskom.Generic.Libs.Properties;
 
     internal static class Program
     {
-        [MiniDump(Text = "Please send a copy of {0} to https://github.com/Elskom/Sdk/issues by making an issue and attaching the log(s) and mini-dump(s).", DumpType = MinidumpTypes.ValidTypeFlags)]
+        [MiniDump(Text = "Please send a copy of {0} to https://github.com/Elskom/Sdk/issues by making an issue and attaching the log(s) and mini-dump(s).", DumpType = DumpType.Full)]
         internal static async Task<int> Main(string[] args)
         {
             MiniDumpAttribute.DumpMessage += MiniDump_DumpMessage;
