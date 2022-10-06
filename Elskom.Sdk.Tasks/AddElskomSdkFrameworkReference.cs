d6221f6b04acbb82df03eafacc8792065739c861
AraHaan <seandhunt_7@yahoo.com>
2022-10-05 23:14:41 -0400

    Add AddElskomSdkFrameworkReference build task. (#264)
    Signed-off-by: AraHaan <seandhunt_7@yahoo.com>
    
    Signed-off-by: AraHaan <seandhunt_7@yahoo.com>

---
+++ Elskom.Sdk.Tasks/AddElskomSdkFrameworkReference.cs
@@ -0,0 +1,19 @@
+﻿namespace Elskom.Sdk.Tasks;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+public class AddElskomSdkFrameworkReference : Task
+{
+    public bool DisableImplicitReference { get; set; }
+    [Output]
+    public ITaskItem[] FrameworkReferences { get; set; } = null!;
+
+    public override bool Execute()
+    {
+        FrameworkReferences = TaskHelpers.ReturnItemOrEmpty(
+            !this.DisableImplicitReference,
+            TaskHelpers.AddFrameworkReference());
+        return true;
+    }
+}
